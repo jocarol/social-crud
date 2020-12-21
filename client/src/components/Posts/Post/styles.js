7cdc58f09a114fbd6849d824e5f11cc781be7d62
joris <joris.carol@gmail.com>
2020-12-21 10:09:05 +0100

    Adding Gif Demo

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -4,7 +4,7 @@ export default makeStyles({
   media: {
     height: 0,
     paddingTop: '56.25%',
-    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    backgroundColor: 'rgba(0, 0, 0, 0.45)',
     '&:hover': {
       backgroundColor: 'unset',
     },
